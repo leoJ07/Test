36bea5fa71d3a03646dbc83db6ccc0777be66018
leoJ07 <joel@achievers.se>
2020-09-24 18:37:36 +0000

    Front End

--- app.js
+++ app.js
@@ -26,12 +26,15 @@ const http = require('http');
 const hostname = '127.0.0.1';
 const port = 3000;
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
-});
+fs.readFile("./frontEnd/index.html", (err, html) => {
+    const server = http.createServer((req, res) => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'text/html');
+        res.write(html);
+        res.end();
+    });
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+    server.listen(port, hostname, () => {
+        console.log(`Server running at http://${hostname}:${port}/`);
+    });
 });
