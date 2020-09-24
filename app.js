cedf8412c46242c1542da5012bb74be1c6f7aecf
leoJ07 <joel@achievers.se>
2020-09-24 15:17:12 +0000

    write/read files and add directorys

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
-console.log("yay");
+var fs = require("fs");
+
+//make a directory
+if(!fs.existsSync("./docs")){
+    fs.mkdir("./docs", (err) => {
+        if(err) console.log(err);
+        console.log("Added the direktry docs")
+    });
+}
+
+//write to files
+fs.writeFile("./docs/myBlog1.txt", "Hello! \nThis is my first blog. ", (err) => {
+    if(err) console.log(err)
+    console.log("Writed to myBlog1.txt")
+});
+
+//read from files
+fs.readFile("./docs/myBlog1.txt", (err, data) => {
+    if(err) console.log(err)
+    console.log(data.toString());
+});
