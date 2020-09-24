a3f0882dd3eb86e9df4f2013761b3d85910da67a
leoJ07 <joel@achievers.se>
2020-09-24 17:11:14 +0000

    web server

--- app.js
+++ app.js
@@ -19,3 +19,19 @@ fs.readFile("./docs/myBlog1.txt", (err, data) => {
     if(err) console.log(err)
     console.log(data.toString());
 });
+
+//making a webserver
+const http = require('http');
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello World');
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
